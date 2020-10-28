299ca09aaa124cd7621f6f61a7b5744929823984
Wale-K <olawale.k17@gmail.com>
2020-10-28 00:58:51 +0000

    The first smash button has been configured so that now when it is clicked in singles mode player 1's score increases and in doubles mode team 1's score increases.

--- src/components/Scoresheet.js
+++ src/components/Scoresheet.js
@@ -7,6 +7,7 @@ import {
   incrementPlayerThreePoints,
   incrementPlayerFourPoints,
   toggleMatchMode,
+  incrementPlayerOneSmashPoint,
 } from "../modules/actions";
 
 const DoublesScoresheet = styled.div`
@@ -17,48 +18,95 @@ const SinglesScoresheet = styled.div`
   display: ${(props) => props.display};
 `;
 
+const NamesInput = styled.input`
+  display: ${(props) => props.display};
+`;
+
 const Scoresheet = (props) => {
+  const playerOneScore = Object.values(props.winners.playerOne).reduce(
+    (x, y) => {
+      return x + y;
+    }
+  );
+
+  const playerTwoScore = Object.values(props.winners.playerTwo).reduce(
+    (x, y) => {
+      return x + y;
+    }
+  );
+
+  const playerThreeScore = Object.values(props.winners.playerThree).reduce(
+    (x, y) => {
+      return x + y;
+    }
+  );
+
+  const playerFourScore = Object.values(props.winners.playerFour).reduce(
+    (x, y) => {
+      return x + y;
+    }
+  );
+
   return (
     <div>
       <button onClick={() => props.switchMode()}>
         {props.isSingles ? "Playing doubles?" : "Playing singles?"}
       </button>
+
       <SinglesScoresheet display={props.isSingles ? "block" : "none"}>
-        {props.playerInfo[0].singles.map((elem) => {
-          return (
-            <p key={elem.name}>
-              {elem.name}: {elem.score}
-            </p>
-          );
-        })}
+        <p>
+          {props.playerInfo[0].name}: {playerOneScore}
+        </p>
+        <p>
+          {props.playerInfo[1].name}: {playerTwoScore}
+        </p>
+
+        {/* Player names in a game of singles */}
+        {/* <p>{props.playerInfo[0].name}</p>
+        <p>{props.playerInfo[1].name}</p> */}
       </SinglesScoresheet>
+
       <DoublesScoresheet display={props.isSingles ? "none" : "block"}>
-        <p>
-          Team 1:{" "}
-          {props.playerInfo[0].singles[0].score +
-            props.playerInfo[0].singles[1].score}
-        </p>
-        <p>
-          Team 2:{" "}
-          {props.playerInfo[0].doubles[0].score +
-            props.playerInfo[0].doubles[1].score}
-        </p>
+        <p>Team 1: {playerOneScore + playerTwoScore}</p>
+        <p>Team 2: {playerThreeScore + playerFourScore}</p>
+
+        {/* Player Names in a game of doubles. */}
+        {/* {props.playerInfo.map((elem) => {
+          return <p key={elem.name}>{elem.name}</p>;
+        })} */}
       </DoublesScoresheet>
+      <p>Please enter the players names:</p>
+
+      <NamesInput />
+      <NamesInput />
+      <NamesInput display={props.isSingles ? "none" : "flex"} />
+      <NamesInput display={props.isSingles ? "none" : "flex"} />
+
+      <button onClick={() => props.playerOneSmash()}>Smash 1</button>
+      <button>Drop 1</button>
+      <button>Long 1</button>
+      <button>Forced 1</button>
+      <button>Smash 2</button>
+      <button>Drop 2</button>
+      <button>Long 2</button>
+      <button>Forced 2</button>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log("this is the state: ", state);
+  //   console.log("this is the state: ", state);
   return {
     isSingles: state.isSingles,
     playerInfo: state.playerInfo,
+    winners: state.winners,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     switchMode: () => dispatch(toggleMatchMode()),
+    playerOneSmash: () => dispatch(incrementPlayerOneSmashPoint()),
   };
 };
 
--- src/modules/reducers.js
+++ src/modules/reducers.js
@@ -4,33 +4,45 @@ import {
   INCREMENET_PLAYER_THREE_POINTS,
   INCREMENET_PLAYER_FOUR_POINTS,
   TOGGLE_MATCH_MODE,
+  INCREMENT_PLAYER_ONE_SMASH_POINT,
 } from "./actions";
 
 export const initialState = {
   isSingles: true,
+
   playerInfo: [
-    {
-      singles: [
-        { name: "Player 1", score: 0 },
-        { name: "Player 2", score: 0 },
-      ],
-      doubles: [
-        { name: "Player 3", score: 0 },
-        { name: "Player 4", score: 0 },
-      ],
-    },
+    { name: "Player 1", score: 0 },
+    { name: "Player 2", score: 0 },
+    { name: "Player 3", score: 0 },
+    { name: "Player 4", score: 0 },
   ],
+  winners: {
+    playerOne: { smashes: 0, drops: 0, long: 0, forced: 0 },
+    playerTwo: { smashes: 0, drops: 0, long: 0, forced: 0 },
+    playerThree: { smashes: 0, drops: 0, long: 0, forced: 0 },
+    playerFour: { smashes: 0, drops: 0, long: 0, forced: 0 },
+  },
 };
 
 export default (state = initialState, action) => {
-  console.log("reducers: ", state);
+  //   console.log("reducers: ", state);
 
   switch (action.type) {
+    case INCREMENT_PLAYER_ONE_SMASH_POINT:
+      return {
+        ...state,
+        winners: {
+          ...state.winners,
+          playerOne: {
+            ...state.winners.playerOne,
+            smashes: state.winners.playerOne.smashes + 1,
+          },
+        },
+      };
+
     case INCREMENET_PLAYER_ONE_POINTS:
       return {
         ...state,
-        teamOneScore: state.teamOneScore + 1,
-        playerOnePoints: state.playerOnePoints + 1,
       };
 
     case INCREMENET_PLAYER_TWO_POINTS:
